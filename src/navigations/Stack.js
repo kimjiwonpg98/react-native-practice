767b2bfed34bb372aac11292a2b29a05636118b2
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-04-16 15:08:59 +0900

    Refactor: stack 내비게이션

--- src/navigations/Stack.js
+++ src/navigations/Stack.js
@@ -3,13 +3,15 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/Home";
 import List from "../screens/List";
 import Item from "../screens/Item";
+import { Platform } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
 const StackNavigation = () => {
   return (
     <Stack.Navigator
-      initialRouteName="Home"
+      initialRouteName='Home'
       screenOptions={{
         cardStyle: { backgroundColor: "#DAD9FF" },
         headerStyle: {
@@ -20,15 +22,39 @@ const StackNavigation = () => {
         },
         headerTitleStyle: { color: "#ffffff", fontSize: 24 },
         headerTitleAlign: "center",
+        headerTitle: ({ style }) => (
+          <MaterialCommunityIcons name='react' style={style} />
+        ),
       }}
     >
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Detail" component={Item} />
+      <Stack.Screen name='Home' component={Home} />
       <Stack.Screen
-        name="List"
+        name='List'
         component={List}
-        options={{ headerTitle: "List Screen" }}
+        options={{
+          headerTitle: "List Screen",
+          headerBackTitle: "Prev",
+          headerBackTitleVisible: true,
+          headerTitleStyle: { fontSize: 24 },
+          headerTintColor: "#e74c3c",
+          headerBackImage: ({ tintColor }) => {
+            const style = {
+              marginRight: 5,
+              marginLeft: Platform.OS === "ios" ? 11 : 0,
+            };
+
+            return (
+              <MaterialCommunityIcons
+                name='keyboard-backspace'
+                size={30}
+                color={tintColor}
+                style={style}
+              />
+            );
+          },
+        }}
       />
+      <Stack.Screen name='Detail' component={Item} />
     </Stack.Navigator>
   );
 };
